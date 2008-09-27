55a8ec51496f0b8ded0f2472986d8a7363271c6c
Gil Abrantes <gil@webreakstuff.com>
2008-09-27 04:38:09 +0100

    added mirrorworker class. did some threading arquitecture work. added year and course tables to yml. added url validation to the rack app, it sends 404 when invalid year/course is requested. Added WocRssBuilder class. added get_file method to WocWorker.

--- woc2rss.rb
+++ woc2rss.rb
@@ -4,10 +4,12 @@ require "net/https"
 require "yaml"
 require "hpricot"
 require "rss/maker"
+require 'rack'
+require 'sqlite3'
 
 class WocFile
 	
-	attr_accessor :download_link, :title, :description, :sec_description
+	attr_accessor :title, :description, :sec_description, :file_id, :file_type, :filename
 	
 end
 
@@ -22,11 +24,7 @@ class WocWorker
 		@conn.verify_mode = OpenSSL::SSL::VERIFY_NONE
 	end
 	
-	def test_method
-		#self
-	end
-	
-	def login(username, password)
+	def login!(username, password)
 		check_value = self.get_check_value
 		login_data = "password=#{password}&username=#{username}&checkValue=#{check_value}"
 		login_header = {
@@ -45,7 +43,6 @@ class WocWorker
 	end
 	
 	def get_generic_list(list, file_id, year_id)
-		#TODO
 		#https://www.dei.uc.pt/weboncampus/class/getxxx.do?idclass=419&idyear=6
 		@materials = Array.new
 		tmp = WocFile.new
@@ -66,7 +63,10 @@ class WocWorker
 				#puts 2
    			when /.*<a href=.*/
 				#download_link
-				tmp.download_link = td.inner_html.match(/.*<a href="(.*)"(.|\n)*/)[1]
+				download_link = td.inner_html.match(/.*<a href="(.*)"(.|\n)*/)[1]
+				link_bits = download_link.match(/\/weboncampus\/getFile.do\?tipo=(\d*)&id=(\d*)/)
+				tmp.file_type = link_bits[1]
+				tmp.file_id = link_bits[2]
    				@materials << tmp
 				#puts 4
 			else
@@ -79,7 +79,6 @@ class WocWorker
 	end
 	
 	def get_projects_list(class_id, year_id)
-		#TODO
 		#https://www.dei.uc.pt/weboncampus/class/getprojects.do?idclass=419&idyear=6
 		response = self.auth_get("/weboncampus/class/getprojects.do?idclass=#{class_id}&idyear=#{year_id}")
 		doc = Hpricot(response.body).search("td[@class='contentcell]")[1].search("table")[1].search("td").each do |td|
@@ -89,6 +88,11 @@ class WocWorker
 			when /\s*<table(.|\n)*/
 				next
 			when /.*<a href=(.|\n)*/
+				#download_link = td.inner_html.match(/.*<a href="(.*)"(.|\n)*/)[1]
+				#link_bits = download_link.match(/\/weboncampus\/getFile.do\?tipo=(\d*)&id=(\d*)/)
+				#tmp.file_type = link_bits[1]
+				#tmp.file_id = link_bits[2]
+				#@materials << tmp
 				next
 			when /^&nbsp;$/
 				next
@@ -100,9 +104,11 @@ class WocWorker
 		end
 	end
 	
-	def get_file
-		#TODO
-		#https://www.dei.uc.pt/weboncampus/getFile.do?tipo=2&id=6836
+	#returns filename and filecontent
+	def get_file(file_id, file_type)
+		response = self.auth_get("/weboncampus/getFile.do?tipo=#{file_type}&id=#{file_id}")
+		filename = response["content-disposition"].match(/filename=(.*)/)[1]
+		return filename, response.body
 	end
 	
 	protected
@@ -139,42 +145,179 @@ class WocWorker
 			end
 		end
 	end
-
 end
+
+
+class MirrorWorker
+	
+	def initialize(db_filename, woc_url, woc_username, woc_password)
+		@download_retries = CONFIG["download_retires"]
+		@mirror_path = CONFIG["mirror_path"]
+		@db = SQLite3::Database.open(db_filename)
+		@woc_worker = WocWorker.new(woc_url)
+		@woc_worker.login!(woc_username, woc_password)
+	end
+	
+	def mirror_file(file_id, file_type)
+		
+		#download the file
+		new_filename, new_filedata = @woc_worker.get_file(file_id, file_type)
+		new_file = File.new(@mirror_path + "/#{new_filename}", 'w')
+		new_file.write(new_filedata)
+		new_file.close
+	rescue => e
+		@download_retries -= 1
+		retry unless @download_retires >= 0
+	end
+		
+		#insert db entry
+		db.execute("INSERT INTO mirror_files (download_link, file_id, file_type, filename) VALUES ('#{}','#{file_id}','#{file_type}','#{new_filename}')")
+		
+		return new_filename
+	end
+	
+	def mirror_filename(file_id, file_type)
+		filename = db.get_first_value("SELECT filename FROM mirror_files WHERE file_type = '#{file_type}' AND file_id = '#{file_id}'")
+		if filename.nil?
+			filename = self.mirror_file(file_id, file_type)
+			return filename
+		else
+			return filename
+		end
+	end
+	
+end
+
 
 #CONFIG = YAML.load(File.open("#{ENV['HOME']}/.deisync.yml"))
 CONFIG = YAML.load(File.open("woc2rss.yml"))
 
-IDS = [ 419, 422, 439, 511, 515 ]
-
+COURSES = CONFIG["courses"]
+YEARS = CONFIG["years"]
 
 @worker = WocWorker.new(CONFIG['url'])
-@worker.login("abrantes", "deftoned")
-@worker.get_generic_list("materialavaliation", "419", "5")[1]
+@worker.login!(CONFIG['username'], CONFIG['password'])
+@worker.get_file(2,6180)
+#b = @worker.get_generic_list("materialavaliation", "419", "5")[1]
+#puts b.file_type
+#puts b.file_id
+#puts b.download_link
 
+class WocRssBuilder
+	
+	def initialize(course_id, year_id)
+		@course_id = course_id
+		@year_id = year_id
+		#woc queue
+		@woc_items = Array.new
+		@woc_mutex = Mutex.new
+		#mirror queue
+		@mirror_items = Array.new
+		@mirror_mutex = Mutex.new
+		#worker instances
+		@mirror_workers = Array.new
+		@woc_workers = Array.new
+		@woc_worker = WocWorker.new(CONFIG['url'])
+		@woc_worker.login!(CONFIG['username'], CONFIG['password']) #FIXME this shouln't be here
+	end
+	
+	def build_rss
+		
+		#spawn one thread to download the material avaliation items
+		@woc_workers << Thread.new(@woc_worker) do |woc_worker|
+			items = @woc_worker.get_generic_list("materialavaliation", @course_id, @year_id)
+			@woc_mutex.synchronize do
+				@woc_items << items
+			end
+		end
+		
+		#spawn one thread to download the material items
+		@woc_workers << Thread.new(@woc_worker) do |woc_worker|
+			items = @woc_worker.get_generic_list("material", @course_id, @year_id)
+			@woc_mutex.synchronize do
+				@woc_items << items
+			end
+		end
+		
+		#spawn one thread to download the project items
+		@woc_workers << Thread.new(@woc_worker) do |woc_worker|
+			items = @woc_worker.get_projects_list(@course_id, @year_id)
+			@woc_mutex.synchronize do
+				@woc_items << items
+			end
+		end
+		
+		#spawn worker_num threads to mirror the woc items
+		@worker_num.times do
+			@mirror_workers << Thread.new(MirrorWorker.new) do |mirror_worker|
+				#consume items from @woc_items
+				#create rss item
+				#produce items to @mirror_items
+			end
+		end
+	end
+	
+	def create_rss
+		version = "2.0"
+		#destination = "test_make.xml"
+		
+		content = RSS::Maker.make(version) do |m|
+			
+			for item in @mirror_items do
+				m.channel.title = "Cadeira X"
+				m.channel.link = "http://www.dei.uc.pt"
+				m.channel.description = "Feed do woc"
+				m.items.do_sort = true
 
-version = "2.0"
-destination = "test_make.xml"
-content = RSS::Maker.make(version) do |m|
-	m.channel.title = "Cadeira X"
-	m.channel.link = "http://www.dei.uc.pt"
-	m.channel.description = "Feed do woc"
-	m.items.do_sort = true
+				i = m.items.new_item
+				i.title = "Hey, i'm the first one"
+				i.link = "http://www.google.com" 
+				i.enclosure.url = "http://blog.makezine.com/crackerBoxAmp09.jpg"
+				i.enclosure.length = "1000"
+				i.enclosure.type = "application/octet-stream"
+				i.date = Time.parse("2008/2/1 23:12")
+
+				i = m.items.new_item
+				i.title = "Hey i'm the second item"
+				i.link = "http://www.sapo.pt"
+				i.date = Time.now
+			end
+		end
+		return content
+	end
 	
-	i = m.items.new_item
-	i.title = "Hey, i'm the first one"
-	i.link = "http://www.google.com" 
-	i.enclosure.url = "http://blog.makezine.com/crackerBoxAmp09.jpg"
-	i.enclosure.length = "1000"
-	i.enclosure.type = "application/octet-stream"
-	i.date = Time.parse("2008/2/1 23:12")
-	
-	i = m.items.new_item
-	i.title = "Hey i'm the second item"
-	i.link = "http://www.sapo.pt"
-	i.date = Time.now
 end
 
-File.open(destination, "w") do |f|
-	f.write(content)
+def translate_year(year_string)
+	if CONFIG["years"][year_string].nil?
+		return nil
+	else
+		return CONFIG["years"][year_string]
+	end
 end
+
+def translate_course(course_string)
+	if CONFIG["courses"][course_string].nil?
+		return nil
+	else
+		return CONFIG["courses"][course_string]
+	end
+end
+
+app = proc do |env|
+	
+	path_bits = env["REQUEST_PATH"].split("/")
+	
+	if path_bits.size == 3
+		year_id = translate_year(path_bits[1])
+		course_id = translate_course(path_bits[2])
+	end
+	
+	unless year_id.nil? or course_id.nil?
+			
+			[200, { 'Content-Type' => 'text/html' }, "valido!"]
+	else
+		[404, { 'Content-Type' => 'text/html'}, "Erro bodes e assim!"]
+	end
+end
+#Rack::Handler::Mongrel.run(app, :Port => 3000)
